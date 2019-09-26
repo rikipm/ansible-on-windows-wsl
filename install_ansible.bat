87ad7433e1272c872e5e4c0dc34125be6e6aaf0e
Rikipm <13685393+rikipm@users.noreply.github.com>
2019-09-26 18:52:07 +0300

    First commit

---
+++ install_ansible.bat
@@ -0,0 +1,12 @@
+@echo off
+net session >nul 2>&1
+if NOT %errorLevel% == 0 (
+    echo You need to launch this bat with admin priveleges
+    pause
+    exit /B 1
+)
+
+@echo on
+wsl sudo apt install ansible
+@echo off
+setx /M PATH "%PATH%%~dp0aliases\"
